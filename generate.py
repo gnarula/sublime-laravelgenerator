bf44b77f8a227836d52edf8d49cf5aa814bbe50b
Gaurav Narula <gnarula94@gmail.com>
2013-03-13 14:30:31 +0530

    Allow two input panels; fix #2

--- generate.py
+++ generate.py
@@ -9,10 +9,13 @@ class GenerateCommand(sublime_plugin.WindowCommand):
     def run(self, *args, **kwargs):
         self.command = kwargs.get('generate', None)
         self.fill_in = kwargs.get('fill_in', 'Enter the resource name')
+        self.accept_fields = kwargs.get('fields', False)
+        self.fields_label = kwargs.get('fields_label', 'Enter the fields')
 
         try:
             # The first folder needs to be the Laravel Project
             self.PROJECT_PATH = self.window.folders()[0]
+            self.command_str = 'php %s/artisan generate:%s ' % (self.PROJECT_PATH, self.command)
 
             if os.path.isfile("%s/artisan" % self.PROJECT_PATH):
                 if self.command in ['model', 'seed', 'test', 'view', 'migration', 'resource']:
@@ -27,17 +30,23 @@ class GenerateCommand(sublime_plugin.WindowCommand):
             print "Please open a Laravel Project"
             sublime.status_message("Please open a Laravel Project")
 
-    def call_artisan(self, resource_name):
-        command_str = "php %s/artisan generate:%s %s" % (self.PROJECT_PATH, self.command, resource_name)
-        args = shlex.split(str(command_str))
-        self.proc = subprocess.Popen(args, shell=False)
-        self.proc_status(resource_name)
-
-    def proc_status(self, resource_name):
-        if self.proc.poll() is None:
-            sublime.set_timeout(lambda: self.proc_status(resource_name), 200)
+    def call_artisan(self, value=''):
+        if self.accept_fields:
+            self.command_str += '%s --fields=' % value
+            self.accept_fields = False
+            self.window.show_input_panel(self.fields_label, '', self.call_artisan, None, None)
         else:
-            if self.proc.returncode == 0:
-                sublime.status_message("%s %s generated!" % (self.command, resource_name))
+            self.command_str += '"%s"' % value
+            print self.command_str
+            args = shlex.split(str(self.command_str))
+            proc = subprocess.Popen(args, shell=False)
+            self.proc_status(proc)
+
+    def proc_status(self, proc):
+        if proc.poll() is None:
+            sublime.set_timeout(lambda: self.proc_status(proc), 200)
+        else:
+            if proc.returncode == 0:
+                sublime.status_message("%s generated successfully!" % self.command)
             else:
-                sublime.status_message("%s failed" % command_str)
+                sublime.status_message("Oh snap! %s failed" % command_str)
